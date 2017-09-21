e914eedf4fa26aae179110c558171a93d1ccde1c
Peter Chung <hkpeterpeter@gmail.com>
2017-09-21 16:25:46 +0800

    Added HKUST Shenzhen BlueBay Alumni Visit

--- src/data/events.js
+++ src/data/events.js
@@ -3,6 +3,7 @@ const makeEvent = (evt, fn) => ({ evt, fn });
 // TODO: refactor the remaining data values
 
 const events = [
+  makeEvent('HKUST Alumni Visit 2017 - Shenzhen BlueBay Incubation Program', 'alumni_visit_hkust_bluebay_incubation_2017.jpg'),
   makeEvent('Judge - A pitching competition of Social-up Youth Program 2017', 'SoInSoGood_HKSTP_2017.jpg'),
   makeEvent('SENG Alumni Social Mixer 2017', 'SENG_Alumni_Social_Mixer_2017.jpg'),
   makeEvent('Interview by TVB about HabHub at RISE 2017', 'HabHub_TVB_Interview_RISE.jpg'),
